7127983666c88f0c47b38e2204e09843af70d514
Dzung Pham <24899880+dzungvpham@users.noreply.github.com>
2022-12-02 20:48:55 -0500

    Use scipy softmax to rank data

--- ranker.py
+++ ranker.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from copy import copy
+from scipy.special import softmax
 
 
 def flip_ranking(ranking: list[int], clicked_idx: int, not_clicked_idx: int) -> list[int]:
@@ -42,26 +43,15 @@ class LinearRanker():
         return X @ self.W
 
     def rank_data(self, X: np.ndarray) -> list[int]:
-        n = X.shape[0]
-        scores = self.forward(X)
-        remaining_cnt = min(self.rank_cnt, n)
-        ranking = []
-        while remaining_cnt > 0:
-            scores[ranking] = np.NINF  # Mask already selected items
-            scores -= np.max(scores)  # Prevent overflow
-            exp_scores = np.exp(scores)
-            probs = (exp_scores / np.sum(exp_scores)).reshape(-1)
-            size = np.minimum(remaining_cnt, np.sum(probs > 0))
-            ranking += self.rng.choice(np.arange(n), size=size,
-                                       p=probs, replace=False, shuffle=False).tolist()
-            remaining_cnt -= size
-
-        return ranking
+        size = min(self.rank_cnt, X.shape[0])
+        probs = softmax(self.forward(X)).reshape(-1)
+        return self.rng.choice(np.arange(X.shape[0]), size=size,
+                               p=probs, replace=False, shuffle=False).tolist()
 
     def calculate_gradient(
             self, X: np.ndarray, ranking: list[int], click_pairs: list[tuple[int, int]]) -> np.ndarray:
 
-        grad = np.zeros_like(self.W) # Initialize gradient
+        grad = np.zeros_like(self.W)  # Initialize gradient
 
         # Calculate Pr(Ranking | Data)
         fX = self.forward(X)
@@ -76,16 +66,16 @@ class LinearRanker():
             score_flipped = calculate_ranking_score(
                 ranking_flipped, fXe, fXe_sum)
             weight = score_flipped / (score_ranking + score_flipped)
-            
+
             # Calculate pair gradient
             e1 = fXe[clicked_idx].item()
             e2 = fXe[not_clicked_idx].item()
             weight *= (e1 * e2) / ((e1 + e2) ** 2)
-            
+
             # Add to model gradient
             grad += weight * (X[clicked_idx] -
                               X[not_clicked_idx]).reshape((-1, 1))
-            
+
             # Unflip ranking
             flip_ranking(ranking_flipped, clicked_idx, not_clicked_idx)
 
